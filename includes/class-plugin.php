2072661d50eb0bc6a8338a799bf438826fefe1c5
zakorski <zakorski@gmail.com>
2025-12-06 17:36:50 -0600

    Add MealsDB_Plugin class for plugin functionality

---
+++ includes/class-plugin.php
@@ -0,0 +1,31 @@
+<?php
+
+class MealsDB_Plugin {
+
+    private static $instance = null;
+
+    public $path;
+    public $url;
+    public $basename;
+
+    public static function init() {
+        if ( self::$instance === null ) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    private function __construct() {
+        $this->path     = plugin_dir_path( __FILE__ ) . '../'; 
+        $this->url      = plugin_dir_url( __FILE__ ) . '../';
+        $this->basename = plugin_basename( dirname( __FILE__, 2 ) . '/meals-db-main.php' );
+    }
+
+    public static function path( $file = '' ) {
+        return self::$instance->path . ltrim( $file, '/' );
+    }
+
+    public static function url( $file = '' ) {
+        return self::$instance->url . ltrim( $file, '/' );
+    }
+}
